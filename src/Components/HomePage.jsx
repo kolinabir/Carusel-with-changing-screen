ec052fc9179871e45b6f9041d3c87c68d3a1083b
kolinabir <knkolin9@gmail.com>
2023-08-09 01:46:12 +0600

    3

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -17,7 +17,11 @@ const HomePage = () => {
       <div className="absolute inset-0 bg-black/50">
         <nav className="flex justify-between items-center py-4 mx-10 text-white ">
           <h1 className="text-3xl font-bold">TravelBoss</h1>
-          <input className="p-2 rounded-lg" type="text" placeholder="Search your destination" />
+          <input
+            className="p-2 rounded-lg"
+            type="text"
+            placeholder="Search your destination"
+          />
           <h3>News</h3>
           <h3>Destination</h3>
           <h3>Blogs</h3>
@@ -25,13 +29,13 @@ const HomePage = () => {
           <button>Login</button>
         </nav>
         {/* Front items and swinger */}
-        <div className="grid grid-cols-10 my-20">
-          <div className="ml-40 text-white col-span-4 space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-10 my-20">
+          <div className="ml-40 text-white col-span-4 space-y-4  order-2 md:order-1">
             <h3 className="text-6xl font-bold">{place?.name}</h3>
             <p>{place?.info}</p>
             <button className="px-3 py-2 rounded bg-yellow-600">Booking</button>
           </div>
-          <div className="text-white col-span-6">
+          <div className="text-white md:col-span-6 order-1 md:order-2">
             <SwiperTry></SwiperTry>
           </div>
         </div>
